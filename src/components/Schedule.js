ba997d815b9bf53a277c24557e19d92588cd5f23
Sadiq <samin.sada@gmail.com>
2020-10-27 13:36:44 -0400

    add sort functionality

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -44,6 +44,48 @@ const useStyles = makeStyles({
     },
 });
 
+function sortClassesHelper(amList) {
+    // sort the classes separately
+    let amIntList = [];
+    amList.forEach(classItem => {
+        let classArr = classItem.startTime.split(" ");
+        let startTime = classArr[0].split(":"); // ["10", "20"], getting rid of the colon in between
+        startTime = startTime[0] + startTime[1]; // "1020"
+        startTime = parseInt(startTime) // 1020
+        amIntList.push(startTime);
+    });
+
+    // sort list
+    for(let i = 0; i < amIntList.length - 1; i++) {
+        for(let j = 1; j < amIntList.length; j++) {
+            if(amIntList[i] > amIntList[j]) {
+                [amIntList[j], amIntList[i]] = [amIntList[i], amIntList[j]]; // swapping
+                [amList[j], amList[i]] = [amList[i], amList[j]]; // swapping initial list
+            }
+        }
+    }
+
+    return amList;
+}
+
+function sortClasses(state) {
+    // first, separate the state into am classes and pm classes
+    let amList = [];
+    let pmList = [];
+    state.forEach(classItem => {
+        let classArr = classItem.startTime.split(" ");
+        let section = classArr[1]; // taking the am/pm part
+        if(section === "AM") amList.push(classItem);
+        if(section === "PM") pmList.push(classItem);
+    });
+
+    amList = sortClassesHelper(amList); 
+    pmList = sortClassesHelper(pmList);
+
+    state = amList.concat(pmList);
+    return state;
+}
+
 
 function Schedule() {
     const classes = useStyles();
@@ -73,6 +115,13 @@ function Schedule() {
                 if(days.includes("TH")) tempThuState.push(currentClass);
                 if(days.includes("F")) tempFriState.push(currentClass);
             });
+
+            tempMonState = sortClasses(tempMonState);
+            tempTueState = sortClasses(tempTueState);
+            tempWedState = sortClasses(tempWedState);
+            tempThuState = sortClasses(tempThuState);
+            tempFriState = sortClasses(tempFriState);
+
             setGridState({
                 monState: tempMonState,
                 tueState: tempTueState,
