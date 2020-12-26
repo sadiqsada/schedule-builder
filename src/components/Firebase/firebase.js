6e40ec20862f14966a371985b10eb311d90276a0
Sadiq <samin.sada@gmail.com>
2020-12-26 13:15:49 -0500

    remove unnecessary files

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,17 +1,5 @@
-import { Portal } from '@material-ui/core';
 import firebase from 'firebase/app';
 import 'firebase/database';
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCwl0ovb07UN7JfJLzC-iANDgZQhcVCPAc",
-//   authDomain: "cse316-hw4-cadac.firebaseapp.com",
-//   databaseURL: "https://cse316-hw4-cadac.firebaseio.com",
-//   projectId: "cse316-hw4-cadac",
-//   storageBucket: "cse316-hw4-cadac.appspot.com",
-//   messagingSenderId: "598968152261",
-//   appId: "1:598968152261:web:2dd483802ee8ff7f6a80ac",
-//   measurementId: "G-WFTB9GMXC5"
-// };
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
